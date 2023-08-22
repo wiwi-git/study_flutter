bc0d10cd9a690bbf4b1b7808caf42a298ff1eaa7
wiwi-git <duse34@hotmail.com>
2023-08-22 20:31:45 +0900

    로그찍어 라이플사이클 확인하기

---
+++ lifecycle_test/android/app/src/main/kotlin/com/example/lifecycle_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lifecycle_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lifecycle_test/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key) {
+    print("MyApp() Constructor");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyApp().build");
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: "LifeCycle Test"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key) {
+    print("MyHomePage() constructor");
+  }
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState() {
+    print("_MyHomePageState() constructor : mounted=${this.mounted}");
+  }
+  final String TAG = "_MyHomePageState";
+
+  int _counter = 0;
+  @override
+  void initState() {
+    print(
+        "$TAG.initState : mounted=${this.mounted} : _counter(${this._counter})");
+    super.initState();
+  }
+
+  @override
+  void reassemble() {
+    print(
+        "$TAG.reassemble :: mounted=${this.mounted} : _counter(${this._counter})");
+    super.reassemble();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print(
+        "$TAG.didChangeDependencies : mounted=${this.mounted} : _counter(${this._counter})");
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    print(
+        "$TAG.dispose : mounted=${this.mounted} : _counter(${this._counter})");
+    super.dispose();
+  }
+
+  @override
+  void deactivate() {
+    print(
+        "$TAG.deactivate : mounted=${this.mounted} : _counter(${this._counter})");
+    super.deactivate();
+  }
+
+  @override
+  void didUpdateWidget(covariant MyHomePage oldWidget) {
+    print(
+        "$TAG.didUpdateWidget : mounted=${this.mounted} : _counter(${this._counter})");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      print(
+          "$TAG.setState() : mounted=${this.mounted} : _counter(${this._counter})");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(
+        "$TAG.build() : mounted=${this.mounted} : _counter(${this._counter})");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
